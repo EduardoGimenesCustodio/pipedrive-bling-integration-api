370b86fdd0bd9a73d06bede11f642ed9febc6fd3
Eduardo Henrique Gimenes Custodio <eduardo.gimenes.custodio@gmail.com>
2022-02-27 08:33:16 -0400

    feat: getWonDealsAndSaveAsDemand method

--- src/app/modules/opportunity/controller/OpportunityController.js
+++ src/app/modules/opportunity/controller/OpportunityController.js
@@ -1,23 +1,43 @@
 const Opportunity = require("../model/Opportunity");
+const GetWonDeals = require("../services/GetWonDeals");
 
 class OpportunityController {
 	async create(req, res) {
-		const date = new Date(req.body.date);
+		try {
+			const date = new Date(req.body.date);
 
-		const dataToCreate = {
-			date,
-			value: req.body.value,
-		};
+			const dataToCreate = {
+				date,
+				value: req.body.value,
+			};
 
-		const data = await Opportunity.create(dataToCreate);
+			const data = await Opportunity.create(dataToCreate);
 
-		return res.json(data);
+			return res.json(data);
+		} catch (error) {
+			return res.status(error.status || 500).json({ error: error.message });
+		}
 	}
 
 	async findAll(req, res) {
-		const data = await Opportunity.find({});
+		try {
+			const data = await Opportunity.find({});
 
-		return res.json(data);
+			return res.json(data);
+		} catch (error) {
+			return res.status(error.status || 500).json({ error: error.message });
+		}
+	}
+
+	async getWonDealsAndSaveAsDemand(req, res) {
+		try {
+			const getWonDeals = new GetWonDeals();
+			const wonDeals = await getWonDeals.run();
+
+			return res.status(201).json(wonDeals);
+		} catch (error) {
+			return res.status(error.status || 500).json({ error: error.message });
+		}
 	}
 }
 
