bed41a23deda6d274a90dac62a1637e56ed136d3
Eduardo Henrique Gimenes Custodio <eduardo.gimenes.custodio@gmail.com>
2022-02-26 20:32:07 -0400

    feat: add home route

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,10 @@ const opportunityRouter = require("./app/modules/opportunity/infra/http/Opportun
 
 const routes = express.Router();
 
+routes.get("/", async (req, res) => {
+	res.send();
+});
+
 routes.use("/opportunity", opportunityRouter);
 
 module.exports = routes;
